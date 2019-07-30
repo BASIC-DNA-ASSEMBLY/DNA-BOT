ccd78785fdb1012ed8314f4401a4f27746632c8f
[Matthew Haines] <mh2210@ic.ac.uk>
2019-07-30 10:30:13 +0100

    Important changes to final_assembly and trans_spot templates.

--- sample/template_ot2_scripts/final_assembly_template.py
+++ sample/template_ot2_scripts/final_assembly_template.py
@@ -16,11 +16,14 @@ def final_assembly(final_assembly_dict, tiprack_num):
     TIPRACK_TYPE = 'opentrons-tiprack-10ul'
     PIPETTE_MOUNT = 'right'
     MAG_PLATE_TYPE = '4ti-0960_FrameStar'
-    MAG_PLATE_POSITION = '4'
+    MAG_PLATE_POSITION = '1'
     TUBE_RACK_TYPE = 'tube-rack_E1415-1500'
     TUBE_RACK_POSITION = '7'
-    DESTINATION_PLATE_TYPE = '4ti-0960_FrameStar'
-    DESTINATION_PLATE_POSITION = '1'
+    DESTINATION_PLATE_TYPE = 'aluminium-block_4ti-0960_FrameStar'
+    TEMPDECK_SLOT = '4'
+    TEMP = 20
+    TOTAL_VOL = 15
+    PART_VOL = 1.5
     MIX_SETTINGS = (1, 3)
 
     # Errors
@@ -34,11 +37,14 @@ def final_assembly(final_assembly_dict, tiprack_num):
                 for slot in slots]
     pipette = instruments.P10_Single(mount=PIPETTE_MOUNT, tip_racks=tipracks)
 
-    # Define Labware
+    # Define Labware and set temperature
     magbead_plate = labware.load(MAG_PLATE_TYPE, MAG_PLATE_POSITION)
     tube_rack = labware.load(TUBE_RACK_TYPE, TUBE_RACK_POSITION)
+    tempdeck = modules.load('tempdeck', TEMPDECK_SLOT)
     destination_plate = labware.load(
-        DESTINATION_PLATE_TYPE, DESTINATION_PLATE_POSITION)
+        DESTINATION_PLATE_TYPE, TEMPDECK_SLOT, share=True)
+    tempdeck.set_temperature(TEMP)
+    tempdeck.wait_for_temp()
 
     # Master mix transfers
     final_assembly_lens = []
@@ -50,20 +56,26 @@ def final_assembly(final_assembly_dict, tiprack_num):
         master_mix_well = master_mix_well_letters[(x - 1) // 6] + str(x - 1)
         destination_inds = [i for i, lens in enumerate(
             final_assembly_lens) if lens == x]
-        destination_wells = np.array([key for key, value in list(final_assembly_dict.items())])
+        destination_wells = np.array(
+            [key for key, value in list(final_assembly_dict.items())])
         destination_wells = list(destination_wells[destination_inds])
         pipette.pick_up_tip()
-        pipette.transfer(10 - x, tube_rack.wells(master_mix_well),
-                         destination_plate.wells(destination_wells), new_tip='never')
+        pipette.transfer(TOTAL_VOL - x * PART_VOL, tube_rack.wells(master_mix_well),
+                         destination_plate.wells(destination_wells),
+                         new_tip='never')
         pipette.drop_tip()
 
     # Part transfers
     for key, values in list(final_assembly_dict.items()):
-        pipette.transfer(1, magbead_plate.wells(values),
-                         destination_plate.wells(key), mix_after=MIX_SETTINGS, new_tip='always')
+        pipette.transfer(PART_VOL, magbead_plate.wells(values),
+                         destination_plate.wells(key), mix_after=MIX_SETTINGS,
+                         new_tip='always')
+
+    tempdeck.deactivate()
 
 
-final_assembly(final_assembly_dict=final_assembly_dict, tiprack_num=tiprack_num)
+final_assembly(final_assembly_dict=final_assembly_dict,
+               tiprack_num=tiprack_num)
 
 for c in robot.commands():
     print(c)
