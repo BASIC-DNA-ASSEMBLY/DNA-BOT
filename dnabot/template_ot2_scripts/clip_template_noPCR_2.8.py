6821504a5efdeeb970ff729ea5a5a023feda792a
Geoff Baldwin <geoffbaldwin@users.noreply.github.com>
2020-12-15 13:31:17 +0000

    Update clip_template_noPCR_2.8.py
    Code has been reordered to better group relevant commands and take the constants out of def clip()
    The intention is to make it a bit clearer and also easier when redefining pipettes or labware

--- dnabot/template_ot2_scripts/clip_template_noPCR_2.8.py
+++ dnabot/template_ot2_scripts/clip_template_noPCR_2.8.py
@@ -2,19 +2,59 @@ from opentrons import protocol_api, simulate
 protocol = simulate.get_protocol_api('2.8')
         
 # Rename to 'clip_template' and paste into 'template_ot2_scripts' folder in DNA-BOT to use
+# Code has been reordered to better group relevant commands and take the constants out of def clip()
 
 #metadata
 metadata = {
      'apiLevel': '2.8',
      'protocolName': 'CLIP_No_Thermocycler',
-     'author': 'Group 4',
+     'author': 'Group 4 & GB',
      'description': 'Implements linker ligation reactions using an opentrons OT-2. This version does not include the Thermocycler module.'}
 
 # example dictionary produced by DNA-BOT for a single construct containing 5 parts, un-comment and run to test the template
-    # clips_dict={"prefixes_wells": ["A8", "A7", "C5", "C7", "C10"], "prefixes_plates": ["2", "2", "2", "2", "2"], "suffixes_wells": ["B7", "C1", "C2", "C3", "B8"], "suffixes_plates": ["2", "2", "2", "2", "2"], "parts_wells": ["E2", "F2", "C2", "B2", "D2"], "parts_plates": ["5", "5", "5", "5", "5"], "parts_vols": [1, 1, 1, 1, 1], "water_vols": [7.0, 7.0, 7.0, 7.0, 7.0]}
+clips_dict={"prefixes_wells": ["A8", "A7", "C5", "C7", "C10"], "prefixes_plates": ["2", "2", "2", "2", "2"], "suffixes_wells": ["B7", "C1", "C2", "C3", "B8"], "suffixes_plates": ["2", "2", "2", "2", "2"], "parts_wells": ["E2", "F2", "C2", "B2", "D2"], "parts_plates": ["5", "5", "5", "5", "5"], "parts_vols": [1, 1, 1, 1, 1], "water_vols": [7.0, 7.0, 7.0, 7.0, 7.0]}
 
 def run(protocol):
 # added run function for API 2.8
+
+    ### Constants - these have been moved out of the def clip() for clarity
+        
+    #Tiprack
+    tiprack_type="opentrons_96_tiprack_10ul"
+    INITIAL_TIP = 'A1'
+    CANDIDATE_TIPRACK_SLOTS = ['3', '6', '9']
+        
+    # Pipettes - pipette instructions in a single location so redefining pipette type is simpler
+    PIPETTE_TYPE = 'p10_single'
+             # API 2 supports gen_1 pipettes like the p10_single
+    PIPETTE_MOUNT = 'right'
+        ### Load Pipette        
+        # checks if it's a P10 Single pipette
+    if PIPETTE_TYPE != 'p10_single':
+        print('Define labware must be changed to use', PIPETTE_TYPE)
+        exit()           
+
+    # Source Plates
+    SOURCE_PLATE_TYPE = 'corning_96_wellplate_360ul_flat'
+            # modified from custom labware as API 2 doesn't support labware.create anymore, so the old add_labware script can't be used
+
+    # Destination Plates
+    DESTINATION_PLATE_TYPE = 'corning_96_wellplate_360ul_flat'
+    DESTINATION_PLATE_POSITION = '1'   
+            # INITIAL_DESTINATION_WELL constant removed, as destination_plate.wells() automatically starts from A1
+        
+    # Tube Rack
+    TUBE_RACK_TYPE = 'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap'
+            # modified from custom labware as API 2 doesn't support labware.create anymore, so the old add_labware script can't be used
+    TUBE_RACK_POSITION = '4'
+    MASTER_MIX_WELL = 'A1'
+    WATER_WELL = 'A2'
+    MASTER_MIX_VOLUME = 20
+        
+    # Mix settings
+    LINKER_MIX_SETTINGS = (1, 3)
+    PART_MIX_SETTINGS = (4, 5)
+    
     def clip(
             prefixes_wells,
             prefixes_plates,
@@ -23,46 +63,9 @@ def run(protocol):
             parts_wells,
             parts_plates,
             parts_vols,
-            water_vols,
-            tiprack_type="opentrons_96_tiprack_10ul"):
-    
+            water_vols):
         
-        ### Constants
-        
-        #Tiprack
-        INITIAL_TIP = 'A1'
-        CANDIDATE_TIPRACK_SLOTS = ['3', '6', '9']
-        
-        # Pipettes
-        PIPETTE_TYPE = 'p10_single'
-             # API 2 supports gen_1 pipettes like the p10_single
-        PIPETTE_MOUNT = 'right'
-           
-
-        # Source Plates
-        SOURCE_PLATE_TYPE = 'corning_96_wellplate_360ul_flat'
-            # modified from custom labware as API 2 doesn't support labware.create anymore, so the old add_labware script can't be used
-
-        # Destination Plates
-        DESTINATION_PLATE_TYPE = 'corning_96_wellplate_360ul_flat'
-        DESTINATION_PLATE_POSITION = '1'   
-            # INITIAL_DESTINATION_WELL constant removed, as destination_plate.wells() automatically starts from A1
-        
-        # Tube Rack
-        TUBE_RACK_TYPE = 'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap'
-            # modified from custom labware as API 2 doesn't support labware.create anymore, so the old add_labware script can't be used
-        TUBE_RACK_POSITION = '4'
-        MASTER_MIX_WELL = 'A1'
-        WATER_WELL = 'A2'
-        MASTER_MIX_VOLUME = 20
-        
-        # Mix settings
-        LINKER_MIX_SETTINGS = (1, 3)
-        PART_MIX_SETTINGS = (4, 5)
-    
-    
-        ### Loading Tiprack
-        
+        ### Loading Tiprack        
         # Calculates whether one, two, or three tipracks are needed, which are in slots 3, 6, and 9 respectively
         total_tips = 4 * len(parts_wells)
         letter_dict = {'A': 0, 'B': 1, 'C': 2,
@@ -71,7 +74,7 @@ def run(protocol):
             13 - int(INITIAL_TIP[1:])) * 8 - letter_dict[INITIAL_TIP[0]]
         if total_tips > tiprack_1_tips:
             tiprack_num = 1 + (total_tips - tiprack_1_tips) // 96 + \
-                (1 if (total_tips - tiprack_1_tips) % 96 > 0 else 0)
+            (1 if (total_tips - tiprack_1_tips) % 96 > 0 else 0)
         else:
             tiprack_num = 1
         slots = CANDIDATE_TIPRACK_SLOTS[:tiprack_num]
@@ -79,14 +82,6 @@ def run(protocol):
         # loads the correct number of tipracks
         tipracks = [protocol.load_labware(tiprack_type, slot) for slot in slots]
             # changed to protocol.load_labware for API 2.8
-
-
-        ### Load Pipette
-        
-        # checks if it's a P10 Single pipette
-        if PIPETTE_TYPE != 'p10_single':
-            print('Define labware must be changed to use', PIPETTE_TYPE)
-            exit()
         
         # Loads pipette according to constants assigned above
         pipette = protocol.load_instrument(PIPETTE_TYPE, mount=PIPETTE_MOUNT, tip_racks=tipracks)
@@ -95,22 +90,8 @@ def run(protocol):
                 # start_at_tip supported by API v1 up to API v2.7, but returns error for 2.8:
                     # 'InstrumentContext' object has no attribute 'start_at_tip'
             # Because of this, ability to specify INITIAL_TIP was removed. Possible future improvement.
-    
-    
-        ### Loading Source Plates
         
-        # Makes a source plate key for where prefixes, suffixes, and parts are located, according to the dictionary generated by the DNA-BOT
-        source_plates = {}
-        source_plates_keys = list(set((prefixes_plates + suffixes_plates + parts_plates)))
-        
-        # Loads plates according to the source plate key
-        for key in source_plates_keys:
-            source_plates[key]=protocol.load_labware(SOURCE_PLATE_TYPE, key)
-                # changed to protocol.load_labware for API 2.8
-        
-        
-        ### Load Destination Plate
-        
+        ### Load Destination Plate      
         # Loads destination plate according to constants assigned above
         destination_plate = protocol.load_labware(DESTINATION_PLATE_TYPE, DESTINATION_PLATE_POSITION)
             # changed to protocol.load_labware for API 2.8
@@ -123,9 +104,7 @@ def run(protocol):
             # Therefore the length arguement replaced by '[0:len(parts_wells)]'
             # Because of this, ability to specify INITIAL_DESTINATION_WELL was removed. Possible future improvement.
         
-        
-        ### Load Tube Rack
-        
+        ### Load Tube Rack        
         # Loads tube rack according to constants assigned above
         tube_rack = protocol.load_labware(TUBE_RACK_TYPE, TUBE_RACK_POSITION)
             # changed to protocol.load_labware for API 2.8
@@ -133,7 +112,16 @@ def run(protocol):
         # Defines positions of master mix and water within the tube rack
         master_mix = tube_rack.wells(MASTER_MIX_WELL)
         water = tube_rack.wells(WATER_WELL)
+   
+         ### Loading Source Plates
+        # Makes a source plate key for where prefixes, suffixes, and parts are located, according to the dictionary generated by the DNA-BOT
+        source_plates = {}
+        source_plates_keys = list(set((prefixes_plates + suffixes_plates + parts_plates)))
         
+        # Loads plates according to the source plate key
+        for key in source_plates_keys:
+            source_plates[key]=protocol.load_labware(SOURCE_PLATE_TYPE, key)
+                # changed to protocol.load_labware for API 2.8      
     
         ### Transfers
         
@@ -141,7 +129,7 @@ def run(protocol):
             # added blowout into destination wells ('blowout_location' only works for API 2.8 and above)
         pipette.pick_up_tip()
         pipette.transfer(MASTER_MIX_VOLUME, master_mix, destination_wells, blow_out=True, blowout_location='destination well', new_tip='never')
-        pipette.return_tip()
+        pipette.drop_tip()
         
         # transfer water into destination wells
             # added blowout into destination wells ('blowout_location' only works for API 2.8 and above)
@@ -150,9 +138,9 @@ def run(protocol):
         #transfer prefixes, suffixes, and parts into destination wells
             # added blowout into destination wells ('blowout_location' only works for API 2.8 and above)
         for clip_num in range(len(parts_wells)):
-            pipette.transfer(1, source_plates[prefixes_plates[clip_num]].wells(prefixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', mix_after=LINKER_MIX_SETTINGS)
-            pipette.transfer(1, source_plates[suffixes_plates[clip_num]].wells(suffixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', mix_after=LINKER_MIX_SETTINGS)
-            pipette.transfer(parts_vols[clip_num], source_plates[parts_plates[clip_num]].wells(parts_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', mix_after=PART_MIX_SETTINGS)
+            pipette.transfer(1, source_plates[prefixes_plates[clip_num]].wells(prefixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=LINKER_MIX_SETTINGS)
+            pipette.transfer(1, source_plates[suffixes_plates[clip_num]].wells(suffixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=LINKER_MIX_SETTINGS)
+            pipette.transfer(parts_vols[clip_num], source_plates[parts_plates[clip_num]].wells(parts_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=PART_MIX_SETTINGS)
        
     # the run function will first define the CLIP function, and then run the CLIP function with the dictionary produced by DNA-BOT
     clip(**clips_dict)
