5f805c6556e40cd72eb0a24e901c1909b3183b72
kbazi-kabbaj <kenza-bk@live.fr>
2021-05-04 09:05:40 +0200

    doc(dnabot): update dnabot informations

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='dnabot',
-    version='0.0.0',
+    version='v1.0.0',
     description='DNA assembly using BASIC on OpenTrons (DNA-BOT)',
     license='MIT',
     author='Matthew C Haines',
-    author_email='thomas.duigou@inrae.fr',
+    author_email='matthew.haines10@imperial.ac.uk',
     packages=find_packages(),
     include_package_data=True,
     keywords=['dnabot'],
-    url='https://github.com/brsynth/DNA-BOT',
+    url='https://github.com/BASIC-DNA-ASSEMBLY/DNA-BOT',
     classifiers=[
         'Topic :: Scientific/Engineering',
     ]
