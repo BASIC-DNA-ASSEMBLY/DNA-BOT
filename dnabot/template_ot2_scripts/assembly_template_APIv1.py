9328e9f80771d941875a5d69cc5614b6df53bd2d
Geoff Baldwin <geoffbaldwin@users.noreply.github.com>
2021-06-22 14:57:19 +0100

    Create assembly_template_APIv1.py

---
+++ dnabot/template_ot2_scripts/assembly_template_APIv1.py
@@ -0,0 +1,80 @@
+from opentrons import labware, instruments, modules, robot
+import numpy as np
+
+
+def final_assembly(final_assembly_dict, tiprack_num, tiprack_type="tiprack-10ul"):
+    """Implements final assembly reactions using an opentrons OT-2.
+
+    Args:
+    final_assembly_dict (dict): Dictionary with keys and values corresponding to destination and associated linker-ligated part wells, respectively.
+    tiprack_num (int): Number of tipracks required during run.
+
+    """
+
+    # Constants
+    CANDIDATE_TIPRACK_SLOTS = ['3', '6', '9', '2', '5', '8', '11']
+    PIPETTE_MOUNT = 'right'
+    MAG_PLATE_TYPE = '4ti-0960_FrameStar'
+    MAG_PLATE_POSITION = '1'
+    TUBE_RACK_TYPE = 'tube-rack_E1415-1500'
+    TUBE_RACK_POSITION = '7'
+    DESTINATION_PLATE_TYPE = 'aluminium-block_4ti-0960_FrameStar'
+    TEMPDECK_SLOT = '4'
+    TEMP = 20
+    TOTAL_VOL = 15
+    PART_VOL = 1.5
+    MIX_SETTINGS = (1, 3)
+
+    # Errors
+    sample_number = len(final_assembly_dict.keys())
+    if sample_number > 96:
+        raise ValueError('Final assembly nummber cannot exceed 96.')
+
+    # Tips and pipette
+    slots = CANDIDATE_TIPRACK_SLOTS[:tiprack_num]
+    tipracks = [labware.load(tiprack_type, slot)
+                for slot in slots]
+    pipette = instruments.P10_Single(mount=PIPETTE_MOUNT, tip_racks=tipracks)
+
+    # Define Labware and set temperature
+    magbead_plate = labware.load(MAG_PLATE_TYPE, MAG_PLATE_POSITION)
+    tube_rack = labware.load(TUBE_RACK_TYPE, TUBE_RACK_POSITION)
+    tempdeck = modules.load('tempdeck', TEMPDECK_SLOT)
+    destination_plate = labware.load(
+        DESTINATION_PLATE_TYPE, TEMPDECK_SLOT, share=True)
+    tempdeck.set_temperature(TEMP)
+    tempdeck.wait_for_temp()
+
+    # Master mix transfers
+    final_assembly_lens = []
+    for values in final_assembly_dict.values():
+        final_assembly_lens.append(len(values))
+    unique_assemblies_lens = list(set(final_assembly_lens))
+    master_mix_well_letters = ['A', 'B', 'C', 'D']
+    for x in unique_assemblies_lens:
+        master_mix_well = master_mix_well_letters[(x - 1) // 6] + str(x - 1)
+        destination_inds = [i for i, lens in enumerate(
+            final_assembly_lens) if lens == x]
+        destination_wells = np.array(
+            [key for key, value in list(final_assembly_dict.items())])
+        destination_wells = list(destination_wells[destination_inds])
+        pipette.pick_up_tip()
+        pipette.transfer(TOTAL_VOL - x * PART_VOL, tube_rack.wells(master_mix_well),
+                         destination_plate.wells(destination_wells),
+                         new_tip='never')
+        pipette.drop_tip()
+
+    # Part transfers
+    for key, values in list(final_assembly_dict.items()):
+        pipette.transfer(PART_VOL, magbead_plate.wells(values),
+                         destination_plate.wells(key), mix_after=MIX_SETTINGS,
+                         new_tip='always')
+
+    tempdeck.deactivate()
+
+
+final_assembly(final_assembly_dict=final_assembly_dict,
+               tiprack_num=tiprack_num)
+
+for c in robot.commands():
+    print(c)
