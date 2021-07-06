ca7ffa9c5b5b7aa52d502d51d2048016ad813b64
Geoff Baldwin <geoffbaldwin@users.noreply.github.com>
2021-07-06 16:58:20 +0100

    Update transformation_template_APIv2.8.py
    Removed old code and update some labware definitions. Moved constants up - may need to come before the def protocol function?

--- dnabot/template_ot2_scripts/transformation_template_APIv2.8.py
+++ dnabot/template_ot2_scripts/transformation_template_APIv2.8.py
@@ -10,6 +10,8 @@ metadata = {
      'protocolName': 'Transformation',
      'description': 'Transformation reactions using an opentrons OT-2 for BASIC assembly.'}
 
+
+
 # Example output produced by DNA-BOT for a single construct, uncomment and run to test the template
 spotting_tuples=[(('A1','B1','C1'), ('A1','B1', 'C1'), (8,8,8))]
 # soc_well='A1'
@@ -18,9 +20,87 @@ spotting_tuples=[(('A1','B1','C1'), ('A1','B1', 'C1'), (8,8,8))]
 #spotting_tuples=[(('A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1'), ('A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2'), ('A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3'), ('A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4'), ('A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5'), ('A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6'), ('A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7'), ('A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8'), ('A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9'), ('A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10'), ('A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10'), (5, 5, 5, 5, 5, 5, 5, 5)), (('A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11'), ('A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11'), (5, 5, 5, 5, 5, 5, 5, 5))]
 soc_well='A1'
 
+
 def run(protocol: protocol_api.ProtocolContext):
 # added run function for API version 2
+
+# Constants
+    CANDIDATE_p20_SLOTS = ['9', '2', '5']
+    CANDIDATE_P300_SLOTS = ['3', '6']
+    p20_TIPRACK_TYPE = 'opentrons_96_tiprack_20ul'
+        # changed from 'tiprack-10ul'
+    P300_TIPRACK_TYPE = 'opentrons_96_tiprack_300ul'
+    P20_MOUNT = 'right'
+    P300_MOUNT = 'left'
+    ASSEMBLY_PLATE_TYPE = '4ti_0960RIG_wellplate_200ul'
+    ASSEMBLY_PLATE_SLOT = '8'
     
+    TRANSFORMATION_PLATE_TYPE = '4ti_0960RIG_wellplate_200ul'
+   
+    SOC_PLATE_TYPE = 'brooks96squaredeepwellstoragemicroplate_96_wellplate_2200ul'
+    SOC_PLATE_SLOT = '7'
+    TUBE_RACK_TYPE = '1.5mlstarlabtubes_24_tuberack_1500ul'
+    TUBE_RACK_SLOT = '11'
+    SPOTTING_WASTE_WELL = 'A1'
+    AGAR_PLATE_TYPE = '4ti_0960RIG_wellplate_200ul'
+        # changed from 'Nunc_Omnitray'
+            # it is a 1 well plate filled with agar;
+            # but for the Opentron to spot in the locations of a 96 wp, it is defined similar to a 96 wp
+            # was previously defined in add.labware.py, API version 2 doesn't support labware.create anymore
+
+      
+        
+        # custom labware made using Opentron's Labware Creator:
+            # external dimensions:
+                # footprint length = 127.76 mm
+                # footrpint width = 85.48 mm
+                # footprint height = 15.70 mm
+                # taken from Thermofisher's documentation for Nunc Omnitray
+                # https://www.thermofisher.com/document-connect/document-connect.html?url=https%3A%2F%2Fassets.thermofisher.com%2FTFS-Assets%2FLSG%2Fmanuals%2FD03023.pdf&title=VGVjaG5pY2FsIERhdGEgU2hlZXQ6IE51bmMgT21uaXRyYXk=
+            # well measurements
+                # depth = 0.01 mm
+                # diameter =  0.01 mm
+                # in old add.labware.py, they were defined as 0, but Labware Creator requires a value >0
+            # spacing 
+                # x-offset = 14.38 mm
+                # y-offset = 11.24 mm
+                # x-spacing = 9.00 mm
+                # y-spacing) = 9.00 mm
+                # taken from Nest 96 well plates
+                # https://labware.opentrons.com/nest_96_wellplate_100ul_pcr_full_skirt/
+        # before using protocol, need to upload the 'nuncomnitray_96_wellplate_0.01ul.json' custom labware file into Opentrons app
+    
+    AGAR_PLATE_SLOT = '1'
+   
+    TEMPDECK_SLOT = '10'
+    # Tiprack slots
+    p20_p300_tiprack_slots = tiprack_slots(spotting_tuples)
+    p20_slots = CANDIDATE_p20_SLOTS[:p20_p300_tiprack_slots[0]]
+    p300_slots = CANDIDATE_P300_SLOTS[:p20_p300_tiprack_slots[1]]
+    
+    # Define labware
+    p20_tipracks = [protocol.load_labware(p20_TIPRACK_TYPE, slot) for slot in p20_slots]
+        # changed to protocol.load_labware for API version 2
+    p300_tipracks = [protocol.load_labware(P300_TIPRACK_TYPE, slot) for slot in p300_slots]
+        # changed to protocol.load_labware for API version 2
+    p20_pipette = protocol.load_instrument('p20_single_gen2', P20_MOUNT, tip_racks=p20_tipracks)
+        # changed to protocol.load_instrument for API version 2
+    p300_pipette = protocol.load_instrument('p300_multi_gen2', P300_MOUNT, tip_racks=p300_tipracks)
+        # changed to protocol.load_instrument for API version 2
+    assembly_plate = protocol.load_labware(ASSEMBLY_PLATE_TYPE, ASSEMBLY_PLATE_SLOT)
+        # changed to protocol.load_labware for API version 2
+    tempdeck = protocol.load_module('tempdeck', TEMPDECK_SLOT)
+    transformation_plate = tempdeck.load_labware(TRANSFORMATION_PLATE_TYPE, TEMPDECK_SLOT)
+        # changed to protocol.load_labware for API version 2
+        # removed share=True, not required in API version 2
+        # removed TEMPDECK_SLOT as it is loaded directly onto temperature module
+    soc_plate = protocol.load_labware(SOC_PLATE_TYPE, SOC_PLATE_SLOT)
+        # changed to protocol.load_labware for API version 2
+    tube_rack = protocol.load_labware(TUBE_RACK_TYPE, TUBE_RACK_SLOT)
+        # changed to protocol.load_labware for API version 2
+    spotting_waste = tube_rack.wells(SPOTTING_WASTE_WELL)
+    agar_plate = protocol.load_labware(AGAR_PLATE_TYPE, AGAR_PLATE_SLOT)
+        # changed to protocol.load_labware for API version 2    
     def generate_transformation_wells(spotting_tuples):
         """
         Evaluates spotting_tuples and returns transformation wells.
@@ -93,10 +173,6 @@ def run(protocol: protocol_api.ProtocolContext):
             # API version2 automatically pauses execution until the set temperature is reached
             # thus it no longer uses .wait_for_temp()
         protocol.pause('Load competent cells, uncap and resume run')
-        # old code:
-            # robot.pause()
-            # robot.comment('Load competent cells, uncap and resume run')
-        # API version 2 uses 'protocol.' instead of 'robot.' and combines '.pause' and '.comment'
     
         # Transfer final assemblies
         p20_pipette.transfer(ASSEMBLY_VOL,
@@ -104,29 +180,13 @@ def run(protocol: protocol_api.ProtocolContext):
                              [transformation_plate.wells_by_name()[well_name] for well_name in transformation_wells], 
                              new_tip='always',
                              mix_after=(MIX_SETTINGS))
-        # old code:
-            # p20_pipette.transfer(ASSEMBLY_VOL,
-                                 # assembly_plate.wells(transformation_wells),
-                                 # transformation_plate.wells(transformation_wells), 
-                                 # new_tip='always',
-                                 # mix_after=(MIX_SETTINGS))
-         # .wells() doesn't take lists as arguements, newer wells_by_name() returns a dictionary
                                  
     
         # Incubate for 20 minutes and remove competent cells for heat shock
         protocol.delay(minutes=INCUBATION_TIME)
-        # old code:
-            # p20_pipette.delay(minutes=INCUBATION_TIME)
-        # API version 2 no longer has .delay() for pipettes, it uses protocol.delay() to pause the entire protocol
-        
-        protocol.pause('Remove transformation reactions, conduct heatshock and replace.')
-        # old code:
-            # robot.pause()
-            # robot.comment('Remove transformation reactions, conduct heatshock in the next heat block and and return the plate.')
-        # API version 2 uses 'protocol.' instead of 'robot.' and combines '.pause' and '.comment'
 
         
-        
+        protocol.pause('Remove transformation reactions, conduct heatshock and replace.')
 
 
     def phase_switch():
@@ -135,13 +195,7 @@ def run(protocol: protocol_api.ProtocolContext):
         
         """
         protocol.pause('Remove final assembly plate. Introduce agar tray and deep well plate containing SOC media. Resume run.')
-        # old code:
-            # def phase_switch(comment='Remove final assembly plate. Introduce agar tray and deep well plate containing SOC media. Resume run.'):
-                # robot.pause()
-                # robot.comment(comment)
-        # API version 2 uses 'protocol.' instead of 'robot.' and combines '.pause' and '.comment'
-
-    
+  
     def outgrowth(
             cols,
             soc_well):
@@ -164,35 +218,20 @@ def run(protocol: protocol_api.ProtocolContext):
     
         # Define wells        
         transformation_cols = [transformation_plate.columns_by_name()[column] for column in cols]
-        # old code:
-            # transformation_cols = transformation_plate.cols(cols)
-        # API version 2 labware use .columns attribute, not .cols
-        # but .columns() doesn't take lists as arguements, newer columns_by_name() returns a dictionary
 
         soc = soc_plate.wells(soc_well)
     
         # Add SOC to transformed cells
         p300_pipette.flow_rate.aspirate = SOC_ASPIRATION_RATE
-        # old code:
-            # p300_pipette.set_flow_rate(aspirate=SOC_ASPIRATION_RATE)
-            # flow rates are set directly in API version 2, brackets not required
         p300_pipette.transfer(SOC_VOL, soc, transformation_cols,
                               new_tip='always', mix_after=SOC_MIX_SETTINGS)
         p300_pipette.flow_rate.aspirate = P300_DEFAULT_ASPIRATION_RATE
-        # old code:
-            # p300_pipette.set_flow_rate(aspirate=P300_DEFAULT_ASPIRATION_RATE)
-            # API version 2 labware use .columns attribute, not .cols
 
         # Incubate for 1 hour at 37 °C
         tempdeck.set_temperature(TEMP)
-        # removed: tempdeck.wait_for_temp()
-            # API version2 automatically pauses execution until the set temperature is reached
-            # thus it no longer uses .wait_for_temp()
             
         protocol.delay(minutes=OUTGROWTH_TIME)
-        # old code:
-            # p300_pipette.delay(minutes=OUTGROWTH_TIME)
-        # API version 2 no longer has .delay() for pipettes, it uses protocol.delay() to pause the entire protocol
+
         
         tempdeck.deactivate()
     
@@ -305,9 +344,6 @@ def run(protocol: protocol_api.ProtocolContext):
     
             # Dispose of dead volume and tip
             p20_pipette.dispense(dead_vol, spotting_waste[0])
-            # old code:
-                # p20_pipette.dispense(dead_vol, spotting_waste)
-                # returns type error because 'target' was a list containing one item (the well location)
 
             p20_pipette.blow_out()
                 # the simple .blow_out command blows out at current position (spotting waste) by defualt
@@ -348,112 +384,12 @@ def run(protocol: protocol_api.ProtocolContext):
             for col in unique_cols:
                 p300_pipette.pick_up_tip()
                 p300_pipette.mix(TRANSFORMATION_MIX_SETTINGS[0], TRANSFORMATION_MIX_SETTINGS[1],transformation_plate.columns_by_name()[col][0])
-                # old code:
-                    # p300_pipette.mix(TRANSFORMATION_MIX_SETTINGS[0], TRANSFORMATION_MIX_SETTINGS[1],transformation_plate.cols(col))
-                    # .columns aka .cols doesn't take lists anymore
-                        # replaced with .columns_by_name
-                    # .mix only takes one location, not several locations
-                        # added [0] to specify only the wells in row A
-                        # is identical for the protocol, as this is using a multi-channel pipette
                 p300_pipette.drop_tip()
             spot_tuple(spotting_tuple)
     
     
     ### Run protocol
     
-    # Constants
-    CANDIDATE_p20_SLOTS = ['9', '2', '5']
-    CANDIDATE_P300_SLOTS = ['3', '6']
-    p20_TIPRACK_TYPE = 'opentrons_96_tiprack_10ul'
-        # changed from 'tiprack-10ul'
-    P300_TIPRACK_TYPE = 'opentrons_96_tiprack_300ul'
-    P20_MOUNT = 'right'
-    P300_MOUNT = 'left'
-    ASSEMBLY_PLATE_TYPE = '4ti_96_wellplate_200ul'
-        # changed from '4ti-0960_FrameStar'
-        # was previously defined in add.labware.py, API version 2 doesn't support labware.create anymore
-    ASSEMBLY_PLATE_SLOT = '8'
-    
-    TRANSFORMATION_PLATE_TYPE = 'opentrons_96_aluminumblock_biorad_wellplate_200ul'
-        # changed from 'Eppendorf_30133366_plate_96'
-            # was previously defined in add.labware.py, API version 2 doesn't support labware.create anymore
-
-        # !!! CURRENT PLATE IS A PLACEHOLDER FOR RUNNING SIMMULATION WITHOUT CUSTOM LABWARE!!!
-        # name made with Opentron Labware Creator = (not yet made)
-        
-        # custom labware not defined: what plate to change to? why is this plate used?
-            # according to add_labware.py, it is a 250ul 96 well plate with spacing identical to the 4ti0960-FrameStar 96 wp
-    
-    SOC_PLATE_TYPE = 'brooks96squaredeepwellstoragemicroplate_96_wellplate_2200ul'
-        # changed from '4ti0136_96_deep-well'
-    SOC_PLATE_SLOT = '7'
-    TUBE_RACK_TYPE = 'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap'
-        # changed from 'tube-rack_E1415-1500'
-    TUBE_RACK_SLOT = '11'
-    SPOTTING_WASTE_WELL = 'A1'
-    AGAR_PLATE_TYPE = '4ti_96_wellplate_200ul'
-        # changed from 'Nunc_Omnitray'
-            # it is a 1 well plate filled with agar;
-            # but for the Opentron to spot in the locations of a 96 wp, it is defined similar to a 96 wp
-            # was previously defined in add.labware.py, API version 2 doesn't support labware.create anymore
-
-        # !!! CURRENT PLATE IS A PLACEHOLDER FOR RUNNING SIMMULATION WITHOUT CUSTOM LABWARE!!!
-        # !!! name made with Opentron Labware Creator = 'nuncomnitray_96_wellplate_0.01ul' !!!!
-        
-        
-        # custom labware made using Opentron's Labware Creator:
-            # external dimensions:
-                # footprint length = 127.76 mm
-                # footrpint width = 85.48 mm
-                # footprint height = 15.70 mm
-                # taken from Thermofisher's documentation for Nunc Omnitray
-                # https://www.thermofisher.com/document-connect/document-connect.html?url=https%3A%2F%2Fassets.thermofisher.com%2FTFS-Assets%2FLSG%2Fmanuals%2FD03023.pdf&title=VGVjaG5pY2FsIERhdGEgU2hlZXQ6IE51bmMgT21uaXRyYXk=
-            # well measurements
-                # depth = 0.01 mm
-                # diameter =  0.01 mm
-                # in old add.labware.py, they were defined as 0, but Labware Creator requires a value >0
-            # spacing 
-                # x-offset = 14.38 mm
-                # y-offset = 11.24 mm
-                # x-spacing = 9.00 mm
-                # y-spacing) = 9.00 mm
-                # taken from Nest 96 well plates
-                # https://labware.opentrons.com/nest_96_wellplate_100ul_pcr_full_skirt/
-        # before using protocol, need to upload the 'nuncomnitray_96_wellplate_0.01ul.json' custom labware file into Opentrons app
-    
-    AGAR_PLATE_SLOT = '1'
-   
-    TEMPDECK_SLOT = '10'
-    # Tiprack slots
-    p20_p300_tiprack_slots = tiprack_slots(spotting_tuples)
-    p20_slots = CANDIDATE_p20_SLOTS[:p20_p300_tiprack_slots[0]]
-    p300_slots = CANDIDATE_P300_SLOTS[:p20_p300_tiprack_slots[1]]
-    
-    # Define labware
-    p20_tipracks = [protocol.load_labware(p20_TIPRACK_TYPE, slot) for slot in p20_slots]
-        # changed to protocol.load_labware for API version 2
-    p300_tipracks = [protocol.load_labware(P300_TIPRACK_TYPE, slot) for slot in p300_slots]
-        # changed to protocol.load_labware for API version 2
-    p20_pipette = protocol.load_instrument('p20_single_gen2', P20_MOUNT, tip_racks=p20_tipracks)
-        # changed to protocol.load_instrument for API version 2
-    p300_pipette = protocol.load_instrument('p300_multi_gen2', P300_MOUNT, tip_racks=p300_tipracks)
-        # changed to protocol.load_instrument for API version 2
-
-    assembly_plate = protocol.load_labware(ASSEMBLY_PLATE_TYPE, ASSEMBLY_PLATE_SLOT)
-        # changed to protocol.load_labware for API version 2
-    tempdeck = protocol.load_module('tempdeck', TEMPDECK_SLOT)
-    transformation_plate = tempdeck.load_labware(TRANSFORMATION_PLATE_TYPE, TEMPDECK_SLOT)
-        # changed to protocol.load_labware for API version 2
-        # removed share=True, not required in API version 2
-        # removed TEMPDECK_SLOT as it is loaded directly onto temperature module
-    soc_plate = protocol.load_labware(SOC_PLATE_TYPE, SOC_PLATE_SLOT)
-        # changed to protocol.load_labware for API version 2
-    tube_rack = protocol.load_labware(TUBE_RACK_TYPE, TUBE_RACK_SLOT)
-        # changed to protocol.load_labware for API version 2
-    spotting_waste = tube_rack.wells(SPOTTING_WASTE_WELL)
-    agar_plate = protocol.load_labware(AGAR_PLATE_TYPE, AGAR_PLATE_SLOT)
-        # changed to protocol.load_labware for API version 2
-
     
     # Register agar_plate for calibration
     p20_pipette.transfer(1, agar_plate.wells('A1'), agar_plate.wells('H12'), trash=False)
