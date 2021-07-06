af35ab5283e0c13ecf58b3dd5b9fb45c1a55a266
Geoff Baldwin <geoffbaldwin@users.noreply.github.com>
2021-07-06 16:46:59 +0100

    Updated some of the labware names and removed old code - CHECK bead container type

--- dnabot/template_ot2_scripts/purification_template_APIv2.8.py
+++ dnabot/template_ot2_scripts/purification_template_APIv2.8.py
@@ -59,7 +59,7 @@ def run(protocol: protocol_api.ProtocolContext):
         MAGDECK_POSITION = '1'
         
         # Mix Plate
-        MIX_PLATE_TYPE = '4ti_96_wellplate_200ul'
+        MIX_PLATE_TYPE = '4ti_0960RIG_wellplate_200ul'
             # modified from custom labware as API 2 doesn't support labware.create anymore, so the old add_labware script can't be used
             # also acts as the type of plate loaded onto the magnetic module
         MIX_PLATE_POSITION = '4'
@@ -70,9 +70,8 @@ def run(protocol: protocol_api.ProtocolContext):
         REAGENT_CONTAINER_POSITION = '7'
         
         # Beads
-        BEAD_CONTAINER_TYPE = '4ti_96_wellplate_200ul'
-            # modified from custom labware as API 2 doesn't support labware.create anymore, so the old add_labware script can't be used
-                # old plate type was '4ti0136_96_deep-well'
+        BEAD_CONTAINER_TYPE = 'brooksreservoirplate_12_wellplate_21000ul'
+                    # old plate type was '4ti0136_96_deep-well'
         BEAD_CONTAINER_POSITION = '8'
         
         # Settings
@@ -118,34 +117,24 @@ def run(protocol: protocol_api.ProtocolContext):
         pipette.dispense_flow_rate=PIPETTE_DISPENSE_RATE
             # for reference: default aspirate/dispense flow rate for p300_multi_gen2 is 94 ul/s
 
-
         ### Define Labware
         
         # Magnetic Module
         MAGDECK = protocol.load_module('magdeck', MAGDECK_POSITION)
-            # changed to protocol.load_module for API verison 2
             # 'magdeck' is the gen 1 magnetic module, use 'magnetic module gen2' for the gen 2 magentic module
                 # if using gen 2 module, need to change settling time! (see comments under Constants)
         MAGDECK.disengage()
-            # disengages the magnets when it is turned on
-            
+            # disengages the magnets when it is turned on          
         mag_plate = MAGDECK.load_labware(MIX_PLATE_TYPE)
-            # old code: 
-                # mag_plate = labware.load(MIX_PLATE_TYPE, MAGDECK_POSITION, share=True)
-            # changed to MAGDECK.load_labware for API version 2
-            # removed MAGDECK_POSITION and share=True as API version 2 uses ModuleContext.load_labware() to load plates directly onto the magnetic module
 
         # Mix Plate
         mix_plate = protocol.load_labware(MIX_PLATE_TYPE, MIX_PLATE_POSITION)
-            # changed to protocol.load_labware for API version 2
-            
+           
         # Reagents
         reagent_container = protocol.load_labware(REAGENT_CONTAINER_TYPE, REAGENT_CONTAINER_POSITION)
-            # changed to protocol.load_labware for API version 2
 
         # Beads Container
         bead_container = protocol.load_labware(BEAD_CONTAINER_TYPE, BEAD_CONTAINER_POSITION)
-            # changed to protocol.load_labware for API version 2
             
             
         ### Calculating Columns
@@ -156,27 +145,15 @@ def run(protocol: protocol_api.ProtocolContext):
         # Columns containing samples in location 1 (magentic module)
             # generates a list of lists: [[A1, B1, C1...], [A2, B2, C2...]...]
         samples = [col for col in mag_plate.columns()[sample_offset : col_num + sample_offset]]
-        # old code:
-            # samples = [col for col in mag_plate.cols()[0 + sample_offset : col_num + sample_offset]]
-            # load_labware needs to take 'columns' attribute instead of just 'cols' in API version 2
-            # removed '0 +' 
             
         # Columns to mix beads and samples in location 4 (mix plate)
         mixing = [col for col in mix_plate.columns()[sample_offset:col_num + sample_offset]]
-        # old code:
-            # mixing = [col for col in mix_plate.columns()[0 + sample_offset:col_num + sample_offset]]
-            # load_labware needs to take 'columns' attribute instead of just 'cols' in API version 2
-            # removed '0 +' 
 
         # Columns to dispense output in location 1 (magnetic module)
             # purified parts are dispensed 6 rows to the right of their initial location
             # this is why the number of samples cannot exceed 48
             
         output = [col for col in mag_plate.columns()[6 + sample_offset:col_num + 6 + sample_offset]]
-        # old code:
-            # output = [col for col in mag_plate.cols()[6 + sample_offset:col_num + 6 + sample_offset]]
-            # load_labware needs to take 'columns' attribute instead of just 'cols' in API version 2
-
     
         ### Defining Wells for Reagents, Liquid Waste, and Beads
         
@@ -184,10 +161,6 @@ def run(protocol: protocol_api.ProtocolContext):
         ethanol = reagent_container.wells(ethanol_well)
         elution_buffer = reagent_container.wells(elution_buffer_well)
         beads = bead_container[BEADS_WELL]
-        # old code:
-            # beads = bead_container.wells(BEADS_WELL)
-            # removed .wells, which created a list containing a single well position instead of just a well position
-
 
         ### Define bead and mix volume
         bead_volume = sample_volume * bead_ratio
@@ -207,20 +180,9 @@ def run(protocol: protocol_api.ProtocolContext):
             pipette.pick_up_tip()
             pipette.aspirate(bead_volume, beads)
             protocol.max_speeds.update(SLOW_HEAD_SPEEDS)
-            # old code:
-                # robot.head_speed(**SLOW_HEAD_SPEEDS, combined_speed=max(SLOW_HEAD_SPEEDS.values()))
-                # robot.head_speed not used in API version 2
-                # replaced with protocol.max_speeds
-            # new code no longer uses the lower value between combined speed or specified speed
-                # just uses each axis' specified speed directly
                 
             # Aspirte samples
             pipette.aspirate(sample_volume + DEAD_TOTAL_VOL, samples[target][0])
-            # old code:
-                # pipette.aspirate(sample_volume + DEAD_TOTAL_VOL, samples[target][0])
-                    # TypeError: location should be a Well or Location, but it is [list of all wells in column 1]
-                # added [0] because samples[target] returned a list of every well in column 1 
-                # the aspirate command for multi channel pipettes takes just one well (the well furthest from the door, row A) as the position of the pipette
             
             # Transfer and mix on mix_plate
             pipette.dispense(total_vol, mixing[target][0])
@@ -231,19 +193,10 @@ def run(protocol: protocol_api.ProtocolContext):
     
             # Dispose of tip
             protocol.max_speeds.update(DEFAULT_HEAD_SPEEDS)
-            # old code:
-                # robot.head_speed(**DEFAULT_HEAD_SPEEDS, combined_speed=max(DEFAULT_HEAD_SPEEDS.values()))
-                # robot.head_speed not used in API version 2
-                # replaced with protocol.max_speeds
-            # new code no longer uses the lower value between combined speed or specified speed
-                # just uses each axis' specified speed directly
             pipette.drop_tip()
     
         # Immobilise sample
         protocol.delay(minutes=incubation_time)
-        # old code:
-            # pipette.delay(minutes=incubation_time)
-        # API version 2 no longer has delay() for pipettes, it uses protocol.delay() to pause the entire protocol
         
         # Transfer beads+samples back to magdeck
         for target in range(int(len(samples))):
@@ -253,9 +206,6 @@ def run(protocol: protocol_api.ProtocolContext):
         # Engagae MagDeck and incubate
         MAGDECK.engage(height=MAGDECK_HEIGHT)
         protocol.delay(minutes=settling_time)
-        # old code:
-            # pipette.delay(minutes=settling_time)
-        # API Version 2 no longer has delay() for pipettes, it uses protocol.delay() to pause the entire protocol
     
         # Remove supernatant from magnetic beads
         for target in samples:
@@ -267,17 +217,12 @@ def run(protocol: protocol_api.ProtocolContext):
             for target in samples:
                 pipette.transfer(ETHANOL_VOL, ethanol, target, air_gap=air_vol)
             protocol.delay(minutes=WASH_TIME)
-            # old code:  
-                # pipette.delay(minutes=WASH_TIME)
             # API Version 2 no longer has delay() for pipettes, it uses protocol.delay() to pause the entire protocol
             for target in samples:
                 pipette.transfer(ETHANOL_VOL + ETHANOL_DEAD_VOL, target, liquid_waste, air_gap=air_vol)
     
         # Dry at room temperature
         protocol.delay(minutes=drying_time)
-        # old code:
-            # pipette.delay(minutes=drying_time)
-        # API Version 2 no longer has delay() for pipettes, it uses protocol.delay() to pause the entire protocol
 
         # Disengage MagDeck
         MAGDECK.disengage()
@@ -292,17 +237,10 @@ def run(protocol: protocol_api.ProtocolContext):
     
         # Incubate at room temperature
         protocol.delay(minutes=elution_time)
-        # old code:
-            # pipette.delay(minutes=elution_time)
-        # API Version 2 no longer has delay() for pipettes, it uses protocol.delay() to pause the entire protocol
-
-    
+   
         # Engage MagDeck (remains engaged for DNA elution)
         MAGDECK.engage(height=MAGDECK_HEIGHT)
         protocol.delay(minutes=ELUTANT_SEP_TIME)
-        # old code:
-            # pipette.delay(minutes=ELUTANT_SEP_TIME)
-        # API Version 2 no longer has delay() for pipettes, it uses protocol.delay() to pause the entire protocol
 
         # Transfer purified parts to a new well
         for target, dest in zip(samples, output):
