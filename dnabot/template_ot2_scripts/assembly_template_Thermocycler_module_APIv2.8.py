2faedc912b0d22433c1432bbacda220de6a4cd94
Thomas Duigou <thomas.duigou@inrae.fr>
2023-01-18 16:25:54 +0100

    style: indent

--- dnabot/template_ot2_scripts/assembly_template_Thermocycler_module_APIv2.8.py
+++ dnabot/template_ot2_scripts/assembly_template_Thermocycler_module_APIv2.8.py
@@ -2,9 +2,9 @@ from opentrons import protocol_api
 import numpy as np
 # metadata
 metadata = {
-'protocolName': 'DNABOT Step 3: Assembly with thermocycler',
-'description': 'DNABOT Step 3: Assembly with thermocycler',
-'apiLevel': '2.8'
+    'protocolName': 'DNABOT Step 3: Assembly with thermocycler',
+    'description': 'DNABOT Step 3: Assembly with thermocycler',
+    'apiLevel': '2.8'
 }
 
 # It is possible to run 88 assemblies with this new module. The heat block module is removed. 
@@ -23,7 +23,9 @@ metadata = {
 # __LABWARES={"p20_single": {"id": "p20_single_gen2"}, "p300_multi": {"id": "p300_multi_gen2"}, "mag_deck": {"id": "magdeck"}, "96_tiprack_20ul": {"id": "opentrons_96_tiprack_20ul"}, "96_tiprack_300ul": {"id": "opentrons_96_tiprack_300ul"}, "24_tuberack_1500ul": {"id": "e14151500starlab_24_tuberack_1500ul"}, "96_wellplate_200ul_pcr_step_14": {"id": "4ti0960rig_96_wellplate_200ul"}, "96_wellplate_200ul_pcr_step_23": {"id": "4ti0960rig_96_wellplate_200ul"}, "agar_plate_step_4": {"id": "4ti0960rig_96_wellplate_200ul"}, "12_reservoir_21000ul": {"id": "4ti0131_12_reservoir_21000ul"}, "96_deepwellplate_2ml": {"id": "4ti0136_96_wellplate_2200ul"}}
 
 def run(protocol: protocol_api.ProtocolContext):
+
     def final_assembly(final_assembly_dict, tiprack_num, tiprack_type=__LABWARES['96_tiprack_20ul']['id']):
+        
             # Constants, we update all the labware name in version 2
             #Tiprack
             CANDIDATE_TIPRACK_SLOTS = ['2', '3', '5', '6', '9']
