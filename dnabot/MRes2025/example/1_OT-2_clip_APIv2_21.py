0723f2f4341f05fc161b4217b475cfac713c1c87
salahhasan2003 <salahhasan2003@gmail.com>
2025-12-02 12:28:56 +0000

    Update 1_OT-2_clip_APIv2_21.py

--- dnabot/MRes2025/example/1_OT-2_clip_APIv2_21.py
+++ dnabot/MRes2025/example/1_OT-2_clip_APIv2_21.py
@@ -12,14 +12,13 @@ metadata = {
      'description': 'Implements linker ligation reactions using an opentrons Flex, including the thermocycler module gen1 or gen2.'
 }
 
+requirements = {"robotType": "OT-2", "apiLevel": "2.21"}
 
 clips_dict={"prefixes_wells": ["A8", "A7", "C5", "C7", "C10", "C12", "C9", "A7", "C10", "C5", "C12", "C6"], "prefixes_plates": ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2"], "suffixes_wells": ["B7", "C1", "C2", "C3", "B8", "B8", "C3", "C2", "C1", "B8", "C1", "C2"], "suffixes_plates": ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2"], "parts_wells": ["E2", "F2", "C2", "B2", "D2", "D2", "B2", "F2", "D2", "C2", "D2", "C2"], "parts_plates": ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5"], "parts_vols": [2.5, 1.0, 1.0, 1.0, 1.25, 1.25, 1.0, 1.0, 1.25, 1.0, 1.25, 1.0], "water_vols": [7.5, 9.0, 9.0, 9.0, 8.75, 8.75, 9.0, 9.0, 8.75, 9.0, 8.75, 9.0]}
 __HARDWARE={"robot_type": {"id": "OT-2"}, "single_pipette": {"id": "p20_single_gen2"}, "single_pipette_mount": {"id": "right"}, "multi_pipette": {"id": "p300_multi_gen2"}, "multi_pipette_mount": {"id": "left"}, "thermocycler": {"id": "thermocyclerModuleV2"}, "mag_deck": {"id": "magnetic module gen1"}}
 __LABWARES={"OT-2_tiprack_20ul": {"id": "opentrons_96_tiprack_20ul"}, "OT-2_tiprack_300ul": {"id": "opentrons_96_tiprack_300ul"}, "24_tuberack_1500ul": {"id": "e14151500starlab_24_tuberack_1500ul"}, "clip_source_plate": {"id": "4ti0960rig_96_wellplate_200ul"}, "clip_plate": {"id": "4ti0960rig_96_wellplate_200ul"}, "mix_plate": {"id": "4ti0960rig_96_wellplate_200ul"}, "final_assembly_plate": {"id": "4ti0960rig_96_wellplate_200ul"}, "transform_plate": {"id": "4ti0960rig_96_wellplate_200ul"}, "agar_plate": {"id": "thermoomnitrayfor96spots_96_wellplate_50ul"}, "12_reservoir_21000ul": {"id": "4ti0131_12_reservoir_21000ul"}, "96_deepwellplate_2ml": {"id": "4ti0136_96_wellplate_2200ul"}, "12_corning_wellplate": {"id": "corning_12_wellplate_6.9ml_flat"}}
 __PARAMETERS={"clip_keep_thermo_lid_closed": {"value": "No", "id": "No"}, "premix_linkers": {"value": "Yes", "id": "Yes"}, "premix_parts": {"value": "Yes", "id": "Yes"}, "linkers_volume": {"value": 20}, "parts_volume": {"value": 20}, "thermo_temp": {"value": 4}, "purif_magdeck_height": {"value": 10.8}, "purif_wash_time": {"value": 0.5}, "purif_bead_ratio": {"value": 1.8}, "purif_incubation_time": {"value": 5}, "purif_settling_time": {"value": 2}, "purif_drying_time": {"value": 5}, "purif_elution_time": {"value": 2}, "transform_incubation_temp": {"value": 4}, "transform_incubation_time": {"value": 20}}
 
-# requirements = {"robotType": __HARDWARE['robot_type']['id'], "apiLevel": "2.21"} #CHANGE 0212
-requirements = {"robotType": "OT-2", "apiLevel": "2.21"}
 
 def run(protocol: protocol_api.ProtocolContext):
     robot_type=__HARDWARE['robot_type']['id']
