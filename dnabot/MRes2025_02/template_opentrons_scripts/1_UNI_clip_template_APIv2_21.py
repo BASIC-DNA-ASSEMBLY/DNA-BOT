8d59159670cd3fe768ab03cb0c364dddee385454
Geoff Baldwin <g.baldwin@imperial.ac.uk>
2026-08-07 13:37:24 +0100

    Update 1_UNI_clip_template_APIv2_21.py
    increased mix  dispense rate

--- dnabot/MRes2025_02/template_opentrons_scripts/1_UNI_clip_template_APIv2_21.py
+++ dnabot/MRes2025_02/template_opentrons_scripts/1_UNI_clip_template_APIv2_21.py
@@ -136,7 +136,7 @@ def run(protocol: protocol_api.ProtocolContext):
             pipette.dispense(aspirate_volume, dispense_location, rate=high)
 
         pipette.aspirate(aspirate_volume, well.bottom(2), rate=slow)
-        pipette.dispense(aspirate_volume, dispense_location, push_out=max(1, aspirate_volume / 5), rate=slow)
+        pipette.dispense(aspirate_volume, dispense_location, push_out=max(1, aspirate_volume / 5), rate=high)
         pipette.move_to(well.top(-5))
         protocol.delay(seconds=1)
         pipette.blow_out()
